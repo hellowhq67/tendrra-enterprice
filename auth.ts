9c64609e1e32a0646d7f13dc6f62de8414be553f
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-02-28 03:47:06 +0600

    Update auth.ts
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- auth.ts
+++ auth.ts
@@ -1,7 +1,36 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google,
+    GoogleProvider({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
   ],
-})
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile?.sub || "";
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.accessToken = token.accessToken as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/sign-up",
+  },
+  secret: process.env.AUTH_SECRET,
+});
