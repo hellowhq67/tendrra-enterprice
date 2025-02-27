07d4fdb551aa2fbea1b8b48715a021439b1a74ea
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-02-28 03:34:38 +0600

    Create next-auth.d.ts
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

---
+++ next-auth.d.ts
@@ -0,0 +1,17 @@
+limport NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+      accessToken?: string;
+    };
+  }
+  interface User {
+    id: string;
+    accessToken?: string;
+  }
+}
