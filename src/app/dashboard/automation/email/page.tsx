10d00272305e9b1b46a61be6d5a6bbd7c8b977f4
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-03-01 02:43:34 +0600

    Update page.tsx
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- src/app/dashboard/automation/email/page.tsx
+++ src/app/dashboard/automation/email/page.tsx
@@ -39,10 +39,7 @@ export default function Page() {
             </BreadcrumbList>
           </Breadcrumb>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
        
-       
-        </div>
       </SidebarInset>
     </SidebarProvider>
   )
