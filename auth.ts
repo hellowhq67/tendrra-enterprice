d3bf6d3c98b28e22d080d5ea9cb291ebdfd3a33d
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-03-01 02:28:17 +0600

    Update auth.ts
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- auth.ts
+++ auth.ts
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.AUTH_SECRET!, // Ensure these are defined!
-      clientSecret: process.env.AUTH_SECRET!, // Ensure these are defined!
+      clientId: process.env.AUTH_GOOGLE_ID, // Ensure these are defined!
+      clientSecret: process.env.AUTH_GOOGLE_SECRET, // Ensure these are defined!
       authorization: {
         params: {
           prompt: "consent",
