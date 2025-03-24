0c8278f94e237c8fbdcd1bab5e4202ec1d0ce5ad
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-03-25 03:04:53 +0600

    Update automation-config-dialog.tsx
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- src/components/workflows/automation-config-dialog.tsx
+++ src/components/workflows/automation-config-dialog.tsx
@@ -23,7 +23,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 
-import { useToast } from "../../hooks/use-toast"
+import { useToast } from "@/hooks/use-toast"
 
 
 
