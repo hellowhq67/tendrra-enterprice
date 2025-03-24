d7cea2850413c8076a30696e0624edbbef0e7283
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-03-24 23:14:16 +0600

    Update authAction.ts
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- src/app/actions/authAction.ts
+++ src/app/actions/authAction.ts
@@ -1,26 +1,19 @@
 "use server"
 
-<<<<<<< HEAD
 
-import { signIn, signOut } from "../../../auth"
-=======
+
+
 import { signIn,signOut } from "../../../auth"
->>>>>>> 157dbbd44305f033179137915d3429a7d0bed63c
 
 
 
 export const SingWithGoogle = async () => {
     await signIn('google')
 }
-<<<<<<< HEAD
+
 
 
 export const SignOut= async()=>{
     await signOut()
 
 }
-=======
-export const SingOut = async () => {
-    await signOut()
-}
->>>>>>> 157dbbd44305f033179137915d3429a7d0bed63c
