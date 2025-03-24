6b6ba73298b2f68679dd6d04419589efd74fe036
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2025-03-24 23:19:29 +0600

    Update index.ts
    Signed-off-by: Lijan Haque <93515488+hellowhq67@users.noreply.github.com>

--- src/remotion/index.ts
+++ src/remotion/index.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
+
 import { registerRoot } from "remotion";
 import { RemotionRoot } from "./Root";
- 
-=======
-import { registerRoot } from "remotion";
-import { RemotionRoot } from "./Root";
- 
->>>>>>> 157dbbd44305f033179137915d3429a7d0bed63c
+
 registerRoot(RemotionRoot);
